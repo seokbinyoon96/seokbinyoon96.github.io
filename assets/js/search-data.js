97bda5cb5e39b702072720c83fdee1f79fab37d6
seokbinyoon96 <112871410+seokbinyoon96@users.noreply.github.com>
2026-08-28 04:23:50 +0000

    Deploying to gh-pages from @ seokbinyoon96/seokbinyoon96.github.io@07b4444a304b3931e77aaba93529d32780420e3c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-news",
+          title: "news",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/news/";
+          },
         },{id: "nav-cv",
           title: "cv",
           description: "",
