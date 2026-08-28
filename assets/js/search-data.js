26edb4acc38087dac111d5aa071550bdd7c52fc0
seokbinyoon96 <112871410+seokbinyoon96@users.noreply.github.com>
2026-08-28 13:38:13 +0000

    Deploying to gh-pages from @ seokbinyoon96/seokbinyoon96.github.io@18d0a0442b0148d6ac1ee1c45d931833a3625332 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -376,7 +376,10 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-i-obtained-my-air-traffic-controller-license-from-the-ministry-of-land-infrastructure-and-transport-south-korea-one-of-the-most-meaningful-milestones-in-my-journey-in-aviation",
+            },},{id: "news-i-was-awarded-the-brain-korea-21-plus-bk21-research-scholarship-funded-by-the-korean-government",
+          title: 'I was awarded the Brain Korea 21 Plus (BK21+) Research Scholarship** funded by...',
+          description: "",
+          section: "News",},{id: "news-i-obtained-my-air-traffic-controller-license-from-the-ministry-of-land-infrastructure-and-transport-south-korea-one-of-the-most-meaningful-milestones-in-my-journey-in-aviation",
           title: 'I obtained my Air Traffic Controller License from the Ministry of Land, Infrastructure...',
           description: "",
           section: "News",},{id: "news-i-received-the-president-s-award-at-korea-aerospace-university-for-winning-first-place-in-the-graduate-research-competition",
