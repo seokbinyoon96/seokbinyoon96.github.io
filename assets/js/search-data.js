02a3605c31d15b151e1b64fb153a33f613351792
seokbinyoon96 <112871410+seokbinyoon96@users.noreply.github.com>
2026-08-28 04:49:28 +0000

    Deploying to gh-pages from @ seokbinyoon96/seokbinyoon96.github.io@798498ef39d96af2bc3068d8e8b5a92877925e88 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -376,7 +376,10 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-paper-on-aircraft-trajectory-prediction-accepted-to-ieee-access",
+            },},{id: "news-best-paper-of-session-award-dasc-2024-for-work-on-uam-fleet-rebalancing-with-real-time-eta-updates",
+          title: 'Best Paper of Session Award, DASC 2024, for work on UAM fleet rebalancing...',
+          description: "",
+          section: "News",},{id: "news-paper-on-aircraft-trajectory-prediction-accepted-to-ieee-access",
           title: 'Paper on aircraft trajectory prediction accepted to IEEE Access.',
           description: "",
           section: "News",},{id: "news-paper-on-aircraft-trajectory-data-augmentation-accepted-to-the-international-journal-of-aeronautical-and-space-sciences",
